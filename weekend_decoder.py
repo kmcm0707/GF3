071696c07dc6a7c023c4e5bd20704cefc8a0925e
Max Iyengar <max20038@gmail.com>
2024-05-10 15:47:12 +0100

    Added prelim steps to decode

---
+++ weekend_decoder.py
@@ -0,0 +1,18 @@
+### WEEKEND DECODER
+
+# Load file1.csv and channel.csv as vectors
+
+# Split into 1056 bit length OFDM 'symbols'
+
+# Remove first 32 elements of each 'symbol'
+
+# DFT of each 'symbol' (...should be complex)
+
+# Divide by DFT of Channel Impulse Response (??)
+
+# For each element, find closest conselation symbol (√2 + √2 j, √2 - √2 j, -√2 - √2 j, - √2 + √2j)
+
+# Match each conselation symbol to the bits (Gray code)
+
+# Convert to .txt (how, not sure!)
+
