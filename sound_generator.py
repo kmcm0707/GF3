14d325bf5171e5bea221fca1811c51538b19638a
Kyle Craig McMillan <kmc0707@hotmail.com>
2024-05-13 13:34:47 +0100

    fix

--- sound_generator.py
+++ sound_generator.py
@@ -3,6 +3,7 @@ import time
 import numpy as np
 import pyaudio
 import scipy.signal as signal
+import pandas as pd
 p = pyaudio.PyAudio()
 time.sleep(1)
 
@@ -12,12 +13,14 @@ duration = 10.0  # in seconds, may be float
 f = 4000.0  # sine frequency, Hz, may be float
 
 # generate samples, note conversion to float32 array
-samples
-samples = (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
+dataframe1 = pd.read_csv('weekend_files/file1.csv', header=None)
+to_decode = dataframe1.to_numpy()
+samples = np.reshape(to_decode, len(to_decode)).astype(np.float32)
+#samples = (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
 
 # generate chirp signal
 t = np.linspace(0, int(duration), int(fs*duration), endpoint=False)
-samples = signal.chirp(t, f0=0, f1=16000, t1=int(duration), method='linear').astype(np.float32)
+#samples = signal.chirp(t, f0=0, f1=16000, t1=int(duration), method='linear').astype(np.float32)
 # per @yahweh comment explicitly convert to bytes sequence
 output_bytes = (volume * samples).tobytes()
 
