ec01be10cd564e1ba9b5a6d0c74dceb208ee6d23
Yuxuan Xie <yx353@cam.ac.uk>
2024-05-13 15:42:43 +0100

    modulator

--- sound_generator.py
+++ sound_generator.py
@@ -1,13 +1,37 @@
 import time
-
 import numpy as np
 import pyaudio
 import scipy.signal as signal
 import pandas as pd
-p = pyaudio.PyAudio()
-time.sleep(1)
 
-volume = 0.5  # range [0.0, 1.0]
+
+
+def generate_sound(samples, volume, fs): # range [0.0, 1.0]
+
+    p = pyaudio.PyAudio()
+    time.sleep(1)
+
+    output_bytes = (volume * samples).tobytes()
+
+    # for paFloat32 sample values must be in range [-1.0, 1.0]
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=fs,
+                    output=True)
+
+    # play. May repeat with different volume values (if done interactively)
+    start_time = time.time()
+    stream.write(output_bytes)
+    print("Played sound for {:.2f} seconds".format(time.time() - start_time))
+
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
+
+
+
+volume = 0.5  
 fs = 44100  # sampling rate, Hz, must be integer
 duration = 10.0  # in seconds, may be float
 f = 4000.0  # sine frequency, Hz, may be float
@@ -22,20 +46,6 @@ samples = np.reshape(to_decode, len(to_decode)).astype(np.float32)
 t = np.linspace(0, int(duration), int(fs*duration), endpoint=False)
 #samples = signal.chirp(t, f0=0, f1=16000, t1=int(duration), method='linear').astype(np.float32)
 # per @yahweh comment explicitly convert to bytes sequence
-output_bytes = (volume * samples).tobytes()
 
-# for paFloat32 sample values must be in range [-1.0, 1.0]
-stream = p.open(format=pyaudio.paFloat32,
-                channels=1,
-                rate=fs,
-                output=True)
 
-# play. May repeat with different volume values (if done interactively)
-start_time = time.time()
-stream.write(output_bytes)
-print("Played sound for {:.2f} seconds".format(time.time() - start_time))
 
-stream.stop_stream()
-stream.close()
-
-p.terminate()
