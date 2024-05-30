47dd586e9c0e38cca8fa995ad77ba4813b0693c5
Max Iyengar <max20038@gmail.com>
2024-05-30 13:03:51 +0100

    First Attempt at LLRs

--- ldpc_jossy/receiver.py
+++ ldpc_jossy/receiver.py
@@ -9,7 +9,9 @@ class receiver(audio_modem):
         self.channel_response()
 
     def channel_response(self):
-        self.channel_response = np.loadtxt('../weekend_files/channel.csv', delimiter = ",", dtype = "float")
+        channel_response = np.loadtxt('../weekend_files/channel.csv', delimiter = ",", dtype = "float")
+        self.channel_response_padded = np.pad(channel_response, (0, self.ofdm_symbol_size - len(channel_response)))
+        self.channel_freq = np.fft.fft(self.channel_response_padded)
 
         # TODO
 
@@ -33,12 +35,9 @@ class receiver(audio_modem):
 
         assert symbols_freq.shape[1] == self.ofdm_symbol_size
 
-        channel_response = np.pad(self.channel_response, (0, self.ofdm_symbol_size - len(self.channel_response)))
-        channel_freq = np.fft.fft(channel_response)
-
         # Divide by DFT of Channel Response:
 
-        recieved_freq = symbols_freq / channel_freq
+        recieved_freq = symbols_freq / self.channel_freq
 
         # Remove complex conjugate bins
         constellations = recieved_freq[0][1:2048]
@@ -48,11 +47,14 @@ class receiver(audio_modem):
         self.constellations = constellations # For showing constellation diagrams
 
         decoded_binary = []
+        llrs = []
+
+        sigma2 = 2 # Sigma squared - TODO Calculate
 
         # Do Inverse Gray Code:
 
         for symbol in constellations:
-            for i in symbol:
+            for index, i in enumerate(symbol):
                 if np.real(i) >= 0 and np.imag(i) >= 0:
                     decoded_binary.extend([0, 0])
                 elif np.real(i) <= 0 and np.imag(i) >= 0:
@@ -64,29 +66,42 @@ class receiver(audio_modem):
                 else:
                     raise Exception("Gray Code Decoding Error")
                 
-        # TODO: State certainty for LDPC (LLhd by distance from axis)
+                # Find LLRs by using distance from axes for soft LDPC decoding:
 
+                # L_1(y) = c_k \times c_k^* y'_i / \sigma ^ 2
 
-        decoded_binary = decoded_binary[:-1966] # TODO find OFDM Padding length automatically
+                l1 = self.channel_freq[index] * np.conj(self.channel_freq[index]) * np.imag(i) / sigma2
 
-        return decoded_binary
+                # L_2(y) = c_k \times c_k^* y'_r / \sigma ^ 2 ... from Jossy LDPC Paper
 
-    def ldpc_decode(self, to_decode):
+                l2 = self.channel_freq[index] * np.conj(self.channel_freq[index]) * np.real(i) / sigma2
+
+                print("Star = ", i, ". l1, l2 =", (l1, l2))
+                llrs.extend([l1.real, l2.real])
+
+        decoded_binary = decoded_binary[:-3296] # TODO find OFDM Padding length automatically
+        llrs = llrs[:-3296] # TODO find OFDM Padding length automatically
+
+        return decoded_binary, llrs
+
+    def ldpc_decode(self, to_decode, llrs):
         decoded = []
 
-        to_decode = np.split(np.array(to_decode), len(to_decode) // self.c.N)
+        llrs = np.split(np.array(llrs), len(llrs) // self.c.N)
 
-        print("Number of OFDM Blocks: ", len(to_decode))
+        print("Number of OFDM Blocks: ", len(llrs))
 
-        for i in to_decode:
-            i = 10 * (0.5 - i) # Do weightings TODO add LLR
+        for i in llrs:
+            # i = 10 * (0.5 - i) # Do weightings
 
             decoded_block, iters = self.c.decode(i)
 
+            print("Iterations ", iters)
+
             decoded_block = decoded_block[:-(self.c.K)] # No idea what the extra information is
-            decoded += ([1 if i < 0 else 0 for i in decoded_block])
-        
-        return decoded[:-296]
+            decoded += ([1 if k < 0 else 0 for k in decoded_block])
+            
+        return decoded[:-392] # TODO find LDPC Padding Length automatically
     
     def decode_text(self, binary_data):
         binary_data = np.array(binary_data).astype("str")
