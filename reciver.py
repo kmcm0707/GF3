884b039581d1420e77bc666c6a7637ff6951d998
Max Iyengar <max20038@gmail.com>
2024-06-01 13:52:47 +0100

    final test recorded

--- reciver.py
+++ reciver.py
@@ -6,8 +6,10 @@ import pandas as pd
 
 def reciver(fs, time, save_file = False, file_name = 'recording.wav'):
     numsamples = int(fs * time)
+    print("recording")
     recording = sd.rec(numsamples, samplerate=fs, channels=1, dtype='float32')
     sd.wait()
+    print("done recording")
     data = np.array(recording)
     data = data.flatten()
     if save_file:
@@ -16,7 +18,7 @@ def reciver(fs, time, save_file = False, file_name = 'recording.wav'):
 
 if __name__ == "__main__":
     fs = 48000
-    time = 3
+    time = 15 # seconds
     recording = reciver(fs, time, save_file = True, file_name = 'recording.csv')
     print(recording)
     plt.plot(recording)
