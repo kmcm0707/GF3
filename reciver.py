648c56e311b976f0d17bb8e979fe78d7efa3745d
Yuxuan Xie <yx353@cam.ac.uk>
2024-06-05 10:50:37 +0100

    l

--- reciver.py
+++ reciver.py
@@ -18,8 +18,8 @@ def reciver(fs, time, save_file = False, file_name = 'recording.wav'):
 
 if __name__ == "__main__":
     fs = 48000
-    time = 20 # seconds
-    recording = reciver(fs, time, save_file = True, file_name = 'malachy_testing4.csv')
+    time = 50 # seconds
+    recording = reciver(fs, time, save_file = True, file_name = 'haoran_testing1.csv')
     print(recording)
     plt.plot(recording)
     plt.show()
